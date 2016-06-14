4deb63136261f647616a0dec8fd373689ba9d6da
Dumpling545 <Yanik_K_2000@mail.ru>
2016-06-14 20:17:58 +0300

    add db abstractions

---
+++ Backend/Backend.DbEntities/UserProjectMembership.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.DbEntities
+{
+    public class UserProjectMembership
+    {
+        public ObjectId UserId { get; set; }
+        public ObjectId ProjectId { get; set; }
+        public bool IsVerified { get; set; }
+    }
+}
---
+++ Backend/Backend.RepositoryAbstractions/IProjectRepository.cs
@@ -0,0 +1,28 @@
+﻿using Backend.DbEntities;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.RepositoryAbstractions
+{
+    interface IProjectRepository
+    {
+        Task<List<Project>> GetProjectsByUserAsync(ObjectId userId);
+        Task<List<ObjectId>> GetUsersInProjectAsync(ObjectId projectId);
+
+        Task AddProjectAsync(Project project);
+        Task<Project> GetProjectAsync(ObjectId projectId);
+        Task ChangeProjectAsync(Project project);
+        Task DeleteProjectAsync(ObjectId projectId);
+
+        Task InviteUserToProjectAsync(ObjectId projectId, ObjectId userId);
+        Task DeleteUserFromProjectAsync(ObjectId projectId, ObjectId userId);
+
+        Task AcceptInvitationToProjectAsync(ObjectId projectId, ObjectId userId);
+        Task RejectInvitationToProjectAsync(ObjectId projectId, ObjectId userId);
+        List<ObjectId> GetAllInvitationsAsync(ObjectId userId);
+    }
+}

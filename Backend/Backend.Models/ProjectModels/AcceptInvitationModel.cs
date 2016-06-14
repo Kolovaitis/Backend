053316625ac2f0af25e5cde68e41d14f0cf9e951
egorix2000 <egorix2000@mail.ru>
2016-06-14 12:28:07 +0300

    add methods

---
+++ Backend/Backend.Models/ProjectModels/AcceptInvitationModel.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Models.ProjectModels
+{
+    public class AcceptInvitationModel
+    {
+        public ObjectId Id { get; set; }
+    }
+}
---
+++ Backend/Backend.Models/ProjectModels/DeleteUserFromProjectModel.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Models.ProjectModels
+{
+    public class DeleteUserFromProjectModel
+    {
+        public string UserEmail { get; set; }
+        public ObjectId ProjectId { get; set; }
+    }
+}

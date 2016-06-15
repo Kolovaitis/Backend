d9a0133bf33b46bbf15e32c8269ecdda9ff1432c
egorix2000 <egorix2000@mail.ru>
2016-06-15 21:52:36 +0300

    add interface methods

---
+++ Backend/Backend.Service/ProjectService/IProjectService.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Models.ProjectModels;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Service.ProjectService
+{
+    public interface IProjectService
+    {
+        Task<ObjectId> AddProject(AddProjectModel model);
+        Task<ICollection<ProjectToSendModel>> getMyProjects(string Email);
+        Task ChangeProject(ChangeProjectModel model);
+        Task DeleteProject(DeleteProjectModel model);
+        Task InviteUserToProject(InviteUserToProjectModel model);
+        Task AcceptInvitation(AcceptInvitationModel model);
+        Task RejectInvitation(RejectInvitationModel model);
+        Task DeleteUserFromProject(DeleteUserFromProjectModel model);
+        Task<ProjectToSendModel> GetProject(GetProjectModel model);
+        Task<ICollection<InvitationModel>> GetAllInvitations(string Email);
+    }
+}
---
+++ Backend/Backend.Service/ProjectService/ProjectService.cs
@@ -0,0 +1,71 @@
+﻿using Backend.RepositoryAbstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using Backend.Models.ProjectModels;
+
+namespace Backend.Service.ProjectService
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectService(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public Task AcceptInvitation(AcceptInvitationModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ObjectId> AddProject(AddProjectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task ChangeProject(ChangeProjectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProject(DeleteProjectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteUserFromProject(DeleteUserFromProjectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<InvitationModel>> GetAllInvitations(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<ProjectToSendModel>> getMyProjects(string Email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProjectToSendModel> GetProject(GetProjectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InviteUserToProject(InviteUserToProjectModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RejectInvitation(RejectInvitationModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

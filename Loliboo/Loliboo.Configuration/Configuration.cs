7710f2c60f953a2f720bae6cef81aac678ccc180
egorix2000 <egorix2000@mail.ru>
2016-06-22 13:09:18 +0300

    add configuration, repository

---
+++ Loliboo/Loliboo.Configuration/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loliboo.Configuration
+{
+    public class Configuration : IConfiguraiton
+    {
+        public string Host => "http://localhost:9000";
+        public string NameDatabase { get; } = "loliboo";
+    }
+}

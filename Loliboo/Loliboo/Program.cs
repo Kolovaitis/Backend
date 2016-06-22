353c8352338cdee4c36d3a355e689460b946867c
egorix2000 <egorix2000@mail.ru>
2016-06-22 12:31:07 +0300

    fix

---
+++ Loliboo/Loliboo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace Loliboo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = new NinjectSettings { LoadExtensions = true };
+            var kernel = new StandardKernel(settings);
+            kernel.Load("XmlConfiguration.xml");
+            var server = kernel.Get<WebServer>();
+            server.Start(() => kernel);
+            Console.ReadKey();
+        }
+    }
+}

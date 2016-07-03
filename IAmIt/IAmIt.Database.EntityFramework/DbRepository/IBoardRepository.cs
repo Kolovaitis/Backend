b573c261294fbac0bc4ebaf63ddbb61171142b31
Dumpling545 <Yanik_K_2000@mail.ru>
2016-07-03 16:59:11 +0300

    Add IBoardRepository, UserBoardMembership, Board

---
+++ IAmIt/IAmIt.Database.EntityFramework/DbRepository/IBoardRepository.cs
@@ -0,0 +1,23 @@
+﻿using IAmIt.DbEntity.DbEntity;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAmIt.Database.EntityFramework.DbRepository
+{
+    public interface IBoardRepository
+    {
+        Task<Board> GetBoardAsync(ObjectId id);
+        Task AddBoardAsync(Board board);
+        Task DeleteBoardAsync(ObjectId id);
+        Task ChangeBoardAsync(Board board);
+        Task<List<Board>> GetBoardsByUserAsync(ObjectId userId);
+        Task<List<Board>> GetBoardsInProjectAsync(ObjectId projectId);
+        Task AddUserToBoardAsync(ObjectId userId, ObjectId boardId);
+        Task DeleteUserFromBoardAsync(ObjectId userId, ObjectId boardId);
+
+    }
+}
---
+++ IAmIt/IAmIt.DbEntity/DbEntity/UserBoardMembership.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IAmIt.DbEntity.DbEntity
+{
+    public class UserBoardMembership
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId UserId { get; set; }
+        public ObjectId BoardId { get; set; }
+    }
+}

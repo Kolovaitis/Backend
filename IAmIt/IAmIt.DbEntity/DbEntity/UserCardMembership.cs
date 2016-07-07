c3d31c7c40548b2a270f0157b724d99ddfbec80b
egorix2000 <egorix2000@mail.ru>
2016-07-07 21:08:14 +0300

    add models and entities

---
+++ IAmIt/IAmIt.DbEntity/DbEntity/UserCardMembership.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace IAmIt.DbEntity.DbEntity
+{
+    public class UserCardMembership
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId UserId { get; set; }
+        public ObjectId CardId { get; set; }
+    }
+}
---
+++ IAmIt/IAmIt.Models/ColumnModels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace IAmIt.Models
+{
+    public class AddColumnModel
+    {
+        public string Name { get; set; }
+        public string BoardId { get; set; }
+    }
+
+    public class DeleteColumnModel
+    {
+        public string ColumnId { get; set; }
+    }
+
+    public class ChangeColumnModel
+    {
+        public string ColumnId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ColumnToSendModel
+    {
+        public string ColumnId { get; set; }
+        public string Name { get; set; }
+        public ICollection<CardToSendLightModel> Cards { get; set; }
+    }
+}
